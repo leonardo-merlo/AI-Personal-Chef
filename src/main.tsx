2530133141f510ea8d3ab449602cb049de3df2f2
leonardo-merlo <leomamerlo@gmail.com>
2025-09-09 15:51:29 -0300

    updated browser icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import avatarLeo from "./assets/avatar-leo.png";
+
+function setFavicon(iconHref: string): void {
+  let linkElement = document.querySelector(
+    "link[rel~='icon']"
+  ) as HTMLLinkElement | null;
+  if (!linkElement) {
+    linkElement = document.createElement("link");
+    linkElement.rel = "icon";
+    document.head.appendChild(linkElement);
+  }
+  linkElement.type = "image/png";
+  linkElement.href = iconHref;
+}
+
+setFavicon(avatarLeo);
 
 createRoot(document.getElementById("root")!).render(<App />);
